82fae9737674042189eb344c4b701e8a614b0d22
RolandMiles <yurov.xander@gmail.com>
2021-10-24 23:36:53 +0300

    Add login/register forms test

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -13,3 +14,10 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                     # открываем страницу
     page.should_be_login_link()     # выполняем метод страницы — проверяем наличие ссылки
+
+
+def test_login_and_register_forms(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                     # открываем страницу
+    page.should_be_login_page()     # выполняем метод страницы — проверяем наличие ссылки
